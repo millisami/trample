b355bcd1a10a905d4e545ea8f62ad7d3a8449f17
John McAliley <john@couponshack.com>
2010-08-20 15:11:34 -0400

    POST only works for :rest.  Need to update code to make work with mechanize (authenticity_tokens and params)

--- config.rb
+++ config.rb
@@ -4,15 +4,17 @@ Trample.configure do
   iterations  10
   rampup_interval 2 #wait time between spawning session threads
   random_wait_interval 0.5 #max wait time between each session requeset.  random between 0 and this value
-  login do
-    post "http://yoursite.com/login" do
-      {:username => User.random.username, :password => "swordfish"}
-    end
-  end
+  
+  ## POST NOT WORKING FOR :mechanize client.  :rest only
+  #login do
+  #  post "http://yoursite.com/login" do
+  #    {:username => User.random.username, :password => "swordfish"}
+  #  end
+  #end
   get  "http://yoursite.com/somewhere"
-  post "http://yoursite.com/something" do
-    {:a_parameter => "a value"}
-  end
+  #post "http://yoursite.com/something" do
+  #  {:a_parameter => "a value"}
+  #end
   get  "http://yoursite.com/someresources/:id" do
     {:id => SomeResource.random.id}
   end
